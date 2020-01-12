93850314c4457158bf90dc6053759a4c159b5d25
Alexyglenn <57509068+Alexyglenn@users.noreply.github.com>
2020-01-12 15:29:50 -0500

    First set

---
+++ main.py
@@ -0,0 +1,9 @@
+take/store images
+analyze image
+remove noise
+analyze edited image (find obect)
+find angle + distance to target
+send info to RIO
+RIO checks if robot is in correct position (relative)
+RIO directs robot to adjust/aim launcher
+RIO activates launcher (if robot in correct position)
